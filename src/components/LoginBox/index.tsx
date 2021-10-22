447958f6a9d51f6f2fa13a0b786cd7ef699872bc
David Barros <dwbarros@gmail.com>
2021-10-22 22:35:21 +0100

    Component MessageList style.

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,5 +1,5 @@
-import { VscGithubInverted } from 'react-icons/vsc'
-import styles from "./styles.module.scss"
+import { VscGithubInverted } from 'react-icons/vsc';
+import styles from './styles.module.scss';
 
 export function LoginBox() {
     return (
@@ -11,4 +11,4 @@ export function LoginBox() {
             </a>
         </div>
     )
-}
+} 
--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,5 +1,41 @@
+import styles from './styles.module.scss';
+
+import logoImg from '../../assets/logo.svg';
+
 export function MessageList() {
     return (
-        <h1>MessageList</h1>
+        <div className={styles.messageListWrapper}>
+            <img className={styles.logoImg} src={logoImg} alt="DoWhile 2021" />
+
+            <ul className={styles.messageList}>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam molestiae blanditiis adipisci provident ad corrupti nostrum ab, libero nisi esse quis consequuntur aliquam facere. Eligendi commodi ipsa aliquid dignissimos nulla!</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/dwbarros.png" alt="Foto de perfil do usuário" />
+                        </div>
+                        <span>David Barros</span>
+                    </div>
+                </li>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam molestiae blanditiis adipisci provident ad corrupti nostrum ab, libero nisi esse quis consequuntur aliquam facere. Eligendi commodi ipsa aliquid dignissimos nulla!</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/dwbarros.png" alt="Foto de perfil do usuário" />
+                        </div>
+                        <span>David Barros</span>
+                    </div>
+                </li>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam molestiae blanditiis adipisci provident ad corrupti nostrum ab, libero nisi esse quis consequuntur aliquam facere. Eligendi commodi ipsa aliquid dignissimos nulla!</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/dwbarros.png" alt="Foto de perfil do usuário" />
+                        </div>
+                        <span>David Barros</span>
+                    </div>
+                </li>
+            </ul>
+        </div>
     )
 }
